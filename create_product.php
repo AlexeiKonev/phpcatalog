c475a70b9099ff7d26fc3cc836b1a4fc609505a5
AlexeiKonev <alexkonev777@gmail.com>
2023-05-01 21:37:16 +0300

    add database

--- create_product.php
+++ create_product.php
@@ -1,12 +1,27 @@
 <?php
+// подключим файлы, необходимые для подключения к базе данных и файлы с объектами
+include_once "config/database.php";
+include_once "objects/product.php";
+include_once "objects/category.php";
 
+// получаем соединение с базой данных
+$database = new Database();
+$db = $database->getConnection();
+
+// создадим экземпляры классов Product и Category
+$product = new Product($db);
+$category = new Category($db);
 // установка заголовка страницы
 $page_title = "Создание товара";
 
 require_once "layout_header.php";
 ?>
 
-<!-- здесь будет контент -->
+    <div class="right-button-margin">
+        <a href="index.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
+    </div>
+
+    <!-- здесь будет html-форма "create product" -->
 
 <?php // подвал
 require_once "layout_footer.php";
